369382f073b74b37706d83f52224b0457060a6b8
JohnElvisMedeirosCosta <jooohn.elvis@hotmail.com>
2022-07-31 13:23:32 -0300

    First Version

---
+++ Cap 07/Exercicios/ferias.py
@@ -0,0 +1,17 @@
+responses = {}
+
+polling_active = True
+
+while polling_active:
+    name = input("Qual é o seu nome? ")
+    message = input("Se pudesse visitar um lugar do mundo, para onde você "
+                    "iria? ")
+    responses[name] = message
+
+    encerrar = input("Gostaria de encerrar a pesquisa? (y/n)")
+
+    if encerrar == 'y':
+        polling_active = False
+
+for name, lugar in responses.items():
+    print(name + ": " + lugar)
